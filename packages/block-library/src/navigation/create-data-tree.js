a370b7e505bdba45539c0bf882f3047dc433efba
Dave Smith <getdavemail@gmail.com>
2020-03-26 16:22:40 +0000

    Document helper function

--- packages/block-library/src/navigation/create-data-tree.js
+++ packages/block-library/src/navigation/create-data-tree.js
@@ -1,3 +1,17 @@
+/**
+ * Creates a nested, hierarchical tree representation from unstructured data that
+ * has an inherent relationship defined between individual items.
+ *
+ * For example, by default, each element in the dataset should have an `id` and
+ * `parent` property where the `parent` property indicates a relationship between
+ * the current item and another item with a matching `id` propertys.
+ *
+ * This is useful for building linked lists of data from flat data structures.
+ *
+ * @param {Array} dataset linked data to be rearranged into a hierarchical tree based on relational fields.
+ * @param {string} id the property which uniquey identifies each entry within the array.
+ * @param {*} relation the property which identifies how the current item is related to other items in the data (if at all).
+ */
 function createDataTree( dataset, id = 'id', relation = 'parent' ) {
 	const hashTable = Object.create( null );
 	const dataTree = [];
